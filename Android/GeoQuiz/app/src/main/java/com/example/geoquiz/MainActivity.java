63481bae620a33787b7b9091dd2c015047296ac3
周福 <602933854@qq.com>
2020-03-06 22:09:24 +0800

    android 3,4章，关于生命周期，屏幕旋转，错误调试

--- Android/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ Android/GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+
     private Button mTrueButton;
     private Button mFalseButton;
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     public void updateQuestionView() {
         int questionId = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(questionId);
+
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -51,7 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_main);
         setTitle(R.string.app_name);
 
@@ -115,5 +123,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+// 周期函数：
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
--- Android/学习笔记/android编程核心指南.md
+++ Android/学习笔记/android编程核心指南.md
@@ -13,6 +13,7 @@
 是用来通知用户的简短弹出消息，用户无需输入或进行任何操作。
 
 Gradle编译android项目：
+
 1. 进入项目目录
 2. ./gradlew tasks //编译项目
 3. ./gradlew installDebug // 安装项目到当前连接的设备上
@@ -30,3 +31,63 @@ Gradle编译android项目：
   - 建立不同像素率的文件夹，将不同像素的图片，放到不同的文件夹中去。图省事，可以只放高像素的图片。
   - 注意点：图片后缀为：.jpg .png .gif 能够自动获得资源id，（注意，文件名必须是小写字母且不能有任何空格符号。）
 - 挑战练习：为 TextView 添加监听器
+
+## 第三章 项目的生命周期以及设备旋转
+
+- andoriod 应用的生命周期：
+
+
+![](img/android编程核心指南/android编程核心指南-2020-03-06-20-02-03.png)
+
+- Log.d()// debug 日志输入，前面是
+
+
+```java
+// 周期函数：
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+```
+
+- LogCat右上角添加过滤器，来查看tag的打印消息
+
+- 在应用程序的设备配置发生改变，andorid就会销毁掉当前的activity，然后创建新的activity，比如：键盘，语言的改变，旋转屏幕。
+- 在旋转之后，或者是暂时退出应用，使用protected void onSaveInstanceState(Bundle outState)保存当前的数据，或者进入暂存状态，（在后台，但是没有被销毁）会自动执行上面的函数。所以覆盖实现，在这个函数中存储数据到Boundle中去。
+
+- 各种日志记录级别：
+
+![](img/android编程核心指南/android编程核心指南-2020-03-06-21-39-32.png)
+
+## 第四章 android应用的调试：
+
+- LogCat查看日志：
+- 记录栈跟踪的诊断性日志：在日志输出函数的函数，第三个参数新建一个 EXception（）
+- 利用调试器设置断点调试
+- 异常断点：就是使用断点来捕获异常，需要在View BreaKpoints 设置异常断点。当有某一个异常发生的时候，就当作断点处理。
+- Android Lint：是Android应用代码的静态分析器。 Analyze Inspect Code运行 Lint
+- 清理项目：Build Clean Project菜单项。
+
+## 第五章：第二个activity
+
