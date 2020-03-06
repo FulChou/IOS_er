4f9b7802c2e47ab54e27ec38374de48c67d5a5f6
周福 <602933854@qq.com>
2020-03-06 16:18:28 +0800

    多媒体window32程序，利用playsound函数，播放音频

---
+++ window程序/SoundPlay/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SoundPlay.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_SOUNDPLAY_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SOUNDPLAY                   107
+#define IDI_SMALL                       108
+#define IDC_SOUNDPLAY                   109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_dis_sound                    32773
+#define ID_stop_sound                   32774
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
