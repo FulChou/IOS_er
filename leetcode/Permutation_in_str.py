ba2c975a061ad2632470bfa5be1d31c560d5b14f
vincent <602933854@qq.com>
2020-10-28 10:32:22 +0800

    滑动窗口2

---
+++ leetcode/Permutation_in_str.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left, right, valid = 0, 0, 0
+        need = {}
+        k_window = {}
+        for c in s1:
+            if c in need:
+                need[c] += 1
+            else:
+                need[c] = 1
+        # print(need)
+        while right < len(s2):
+            c = s2[right]
+            right += 1
+            if c in need:
+                if c in k_window:
+                    k_window[c] += 1
+                else: k_window[c] = 1
+                if k_window[c] == need[c]:
+                    valid += 1
+            while (right - left) >= len(s1):
+                if valid == len(need):
+                    return True
+                else:
+                    c = s2[left]
+                    left += 1
+                    if c in k_window:
+                        if k_window[c] == need[c]:
+                            valid -= 1
+                        k_window[c] -= 1
+        return False
